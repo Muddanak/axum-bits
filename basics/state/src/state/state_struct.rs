5e5d9501ea2158d387d25a45bee7a97ace5cbd32
Steve <MuddobberSK@gmail.com>
2024-07-01 11:22:08 -0500

    Cleaned up state_struct for visibility

--- basics/state/src/state/state_struct.rs
+++ basics/state/src/state/state_struct.rs
@@ -3,11 +3,16 @@ pub(crate) struct StateInfo {
     pub name: String,
     pub version: String,
     pub secret_number: u8,
-    pub vec_list: Vec<u8>
+    pub vec_list: Vec<u8>,
 }
 
 impl Default for StateInfo {
     fn default() -> Self {
-        Self { name: String::from("basic-state example"), version: String::from("v1.3.3.7"), secret_number: 42, vec_list: vec![72, 101, 108, 108, 111] }
+        Self {
+            name: String::from("basic-state example"),
+            version: String::from("v1.3.3.7"),
+            secret_number: 42,
+            vec_list: vec![72, 101, 108, 108, 111],
+        }
     }
 }
