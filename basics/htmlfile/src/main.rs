4aa52638d8f99d6b3d09564bf004ec25df81c101
Steve <MuddobberSK@gmail.com>
2024-07-09 14:08:39 -0500

    Add in one way to handle and serve an HTML file through the plain index

---
+++ basics/htmlfile/src/main.rs
@@ -0,0 +1,31 @@
+/// Exhibiting a basic method to handle a simple server implementation of Axum
+/// This implementation introduces tower_http to provide the ServeFile service
+use axum::{response::Html, Router};
+use tower_http::services::ServeFile;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Basic webserver with one default route via GET request
+    // Contains a global fallback for a 404 error if the route isn't found
+    let webserver = Router::new()
+        // Basic routing service that returns a stored html file
+        .route_service("/", ServeFile::new("pages/index.html"))
+        // Global 404 route and handler closure
+        .fallback(|| async move { Html("404: Not found") });
+
+    // Establish a listening socket
+    // Gracefully errors out if it can't do so
+    let weblistener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
+
+    // Executes the server over the listening socket
+    // Gracefully errors out if there are any problems
+    axum::serve::serve(weblistener, webserver).await?;
+
+    Ok(())
+}
+
+/* /// The function called for the default index state
+async fn index_for_example() -> &'static str {
+    "All good"
+}
+ */
